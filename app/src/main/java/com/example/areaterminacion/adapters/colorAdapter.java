cf0f3b2e6ad4ac032d380802751a4cdb7597ef55
Diego Vasquez <diegovasquezcolombia@gmail.com>
2019-09-18 16:55:47 -0500

    Lisat de tallas y paquete adapters creado, con clases tallaAdapter y colorAdapter

---
+++ app/src/main/java/com/example/areaterminacion/adapters/colorAdapter.java
@@ -0,0 +1,7 @@
+package com.example.areaterminacion.adapters;
+
+public class colorAdapter {
+    public static final String[] colorNames = {
+        "Crudo702", "Blanco1004", "Rosado304", "Palo de rosa326"
+    };
+}
---
+++ app/src/main/java/com/example/areaterminacion/adapters/tallaAdapter.java
@@ -0,0 +1,8 @@
+package com.example.areaterminacion.adapters;
+
+public class tallaAdapter {
+    public static final String[] tallas = {
+            "2", "4","6", "8", "10", "12", "14", "16", "18", "XS", "S", "M", "L", "XL", "2XL","3XL", "4XL", "5XL"
+    };
+
+}
